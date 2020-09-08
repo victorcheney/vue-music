e94ce5ee11c5b6cc9a241d1c0e4a5f9a4f86b907
chenfengtao <cft12534@cnki.net>
2020-09-08 14:13:34 +0800

    chore: 修改环境配置

--- config/index.js
+++ config/index.js
@@ -109,7 +109,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/vuemusic/',
 
     /**
      * Source Maps
--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,9 @@ import SingerDetil from 'components/singer-detail/singer-detail'
 Vue.use(Router)
 
 export default new Router({
+  base: process.env.NODE_ENV === 'production'
+    ? '/vuemusic/'
+    : '/',
   routes: [
     {
       path: '/',
